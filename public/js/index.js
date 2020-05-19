4451f4291d9519c5f03ee8fb6057fef5137eb9c8
Robert L. Kushner IV <robertkushner4@gmail.com>
2020-05-19 16:06:51 -0400

    better scrolling

---
+++ public/js/index.js
@@ -0,0 +1,11 @@
+const about = document.querySelector('#about');
+
+about.addEventListener("click", function(e) {
+    preventDefault();
+});
+
+const contact = document.querySelector('#contact');
+
+contact.addEventListener("click", function(e) {
+    preventDefault();
+});
